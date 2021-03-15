66dd8656427dc3d2aa44b1aaddb3d15c7e2c2d05
Yazan Ibesh <yazanibesh@Yazans-MacBook-Pro.local>
2021-03-15 14:59:21 +0100

    configured react project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+;
+
+function App() {
+  return (
+    <div>
+       
+
+      <h1>
+        Test 1
+        Test 2  
+      </h1>
+
+
+
+    </div>
+  );
+}
+
+export default App;
